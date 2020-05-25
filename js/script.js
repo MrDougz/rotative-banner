574f9cb01897cad3f9c679bc217a3f299cd8301e
Douglas Miranda Santos <mrdougz2006@gmail.com>
2020-05-25 19:38:58 -0300

    start basic

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let time = 2000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll('#slider img')
+    max = images.length;
+
+function start(){
+    setInterval(() => {}, time)
+}
+
+window.addEventListener("load", start)
