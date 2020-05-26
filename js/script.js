579bf892caf74c2074c13635475ea1fe2820ddf2
Douglas Miranda Santos <mrdougz2006@gmail.com>
2020-05-26 08:11:37 -0300

    logic finished

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-let time = 2000,
+let time = 5000,
     currentImageIndex = 0,
     images = document.querySelectorAll('#slider img')
     max = images.length;
 
 function nextImage(){
-    
+
+    images[currentImageIndex].classList.remove('selected')
+    currentImageIndex++
+
+    if(currentImageIndex >= max){
+        currentImageIndex = 0
+    }
+    images[currentImageIndex].classList.add('selected')
 }
 
 function start(){
     setInterval(() => {
-        console.log("função rodando!")
+        nextImage()
     }, time)
 }
 
